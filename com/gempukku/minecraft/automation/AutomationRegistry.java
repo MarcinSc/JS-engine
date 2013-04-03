69a6031a0fd68923efcd977fd5ab03d89fba1a34
marcins78@gmail.com <marcins78@gmail.com@4b48f6f3-f5b4-2729-cc71-6049987250f3>
2013-04-03 15:01:01 +0000

    Working on OS and modules.

--- com/gempukku/minecraft/automation/AutomationRegistry.java
+++ com/gempukku/minecraft/automation/AutomationRegistry.java
@@ -1,15 +1,24 @@
 package com.gempukku.minecraft.automation;
 
 import com.gempukku.minecraft.automation.computer.ComputerData;
+import com.gempukku.minecraft.automation.computer.ComputerModule;
+import net.minecraft.item.Item;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class AutomationRegistry {
-    private static Map<Integer, ComputerData> _computerRegistry = new ConcurrentHashMap<Integer, ComputerData>();
-    private static int _nextInt;
+    private static AutomationRegistry _instance = new AutomationRegistry();
 
-    public static ComputerData getComputerData(int id) {
+    private Map<Integer, ComputerData> _computerRegistry = new ConcurrentHashMap<Integer, ComputerData>();
+    private Map<Item, ComputerModule> _computerModules = new ConcurrentHashMap<Item, ComputerModule>();
+    private int _nextInt;
+
+    public void registerComputerModule(Item item, ComputerModule computerModule) {
+        _computerModules.put(item, computerModule);
+    }
+
+    public ComputerData getComputerData(int id) {
         return _computerRegistry.get(id);
     }
 }
