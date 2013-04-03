ff0d923a66e8ea632979463787624cc13f23db62
marcins78@gmail.com <marcins78@gmail.com@4b48f6f3-f5b4-2729-cc71-6049987250f3>
2013-04-03 14:58:10 +0000

    Working on OS and modules.

--- com/gempukku/minecraft/automation/computer/JavaFunctionExecutable.java
+++ com/gempukku/minecraft/automation/computer/JavaFunctionExecutable.java
@@ -8,7 +8,7 @@ import java.util.Map;
 
 public abstract class JavaFunctionExecutable implements FunctionExecutable {
     @Override
-    public Execution createExecution(CallContext callContext) {
+    public final Execution createExecution(CallContext callContext) {
         return new SimpleExecution() {
             @Override
             protected ExecutionProgress execute(ExecutionContext context) throws ExecutionException {
@@ -26,7 +26,24 @@ public abstract class JavaFunctionExecutable implements FunctionExecutable {
         };
     }
 
-    protected abstract Object executeFunction(ComputerData computer, Map<String, Variable> parameters) throws ExecutionException;
-
+    /**
+     * Returns duration of the operation in computer cycles. Used to effectively throttle computer programs.
+     * @return Duration in computer cycles.
+     */
     protected abstract int getDuration();
+
+    /**
+     * Executes this function, gets passed an instance of the computer this function is executed on, as well as
+     * parameters passed to the function, as defined by getParameterNames method in this class. The returned object
+     * will be placed into context that called this function. It is advisable to use only basic objects as return values.
+     * Numbers (int, float), booleans, Strings and null.
+     * If an execution of the program should be stopped due to a fatal exception, ExecutionException should be thrown by
+     * the method.
+     * @param computer Computer this function is executed on.
+     * @param parameters Parameters that were sent to this function.
+     * @return Object that has to be set in context of the caller (return value).
+     * @throws ExecutionException Fatal exception that will be communicated to the computer console. When thrown,
+     * the execution of the program will stop.
+     */
+    protected abstract Object executeFunction(ComputerData computer, Map<String, Variable> parameters) throws ExecutionException;
 }
