966964e135d972a69005ef7a01149e466636cd60
marcins78@gmail.com <marcins78@gmail.com@4b48f6f3-f5b4-2729-cc71-6049987250f3>
2013-04-09 11:07:23 +0000

    Working on GUI.

--- com/gempukku/minecraft/automation/ComputerGuiHandler.java
+++ com/gempukku/minecraft/automation/ComputerGuiHandler.java
@@ -6,12 +6,16 @@ import net.minecraft.world.World;
 
 public class ComputerGuiHandler implements IGuiHandler {
     @Override
-    public Object getClientGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
+    public Object getClientGuiElement(int id, EntityPlayer player, World world, int x, int y, int z) {
+        if (id == 1)
+            return new ComputerModsGui(player.inventory, (ComputerTileEntity) world.getBlockTileEntity(x, y, z));
         return null;
     }
 
     @Override
-    public Object getServerGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
+    public Object getServerGuiElement(int id, EntityPlayer player, World world, int x, int y, int z) {
+        if (id == 1)
+            return new ComputerModsContainer(player.inventory, (ComputerTileEntity) world.getBlockTileEntity(x, y, z));
         return null;
     }
 }
