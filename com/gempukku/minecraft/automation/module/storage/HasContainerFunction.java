89b116f6da852f85735b384cf5e9ffb6e251e47f
marcins78@gmail.com <marcins78@gmail.com@4b48f6f3-f5b4-2729-cc71-6049987250f3>
2013-04-16 11:33:37 +0000

    Started work on Storage module - for storing items in computer, as well as manipulating items in the world (other containers).

--- com/gempukku/minecraft/automation/module/storage/HasContainerFunction.java
+++ com/gempukku/minecraft/automation/module/storage/HasContainerFunction.java
@@ -29,17 +29,21 @@ public class HasContainerFunction extends JavaFunctionExecutable {
         final Variable sideVar = parameters.get("side");
 
         if (sideVar.getType() != Variable.Type.STRING)
-            throw new ExecutionException("Expected front, left or right in hasContainer function");
+            throw new ExecutionException("Expected front, top, bottom, left or right in hasContainer function");
 
         String side = (String) sideVar.getValue();
-        if (!side.equals("front") || !side.equals("left") || !side.equals("right"))
-            throw new ExecutionException("Expected front, left or right in hasContainer function");
+        if (!side.equals("front") || !side.equals("left") || !side.equals("right") || !side.equals("top") || !side.equals("bottom"))
+            throw new ExecutionException("Expected front, top, bottom, left or right in hasContainer function");
 
         int lookAt = facing;
         if (side.equals("left"))
             lookAt = BoxSide.getLeft(facing);
         else if (side.equals("right"))
             lookAt = BoxSide.getRight(facing);
+        else if (side.equals("top"))
+            lookAt = BoxSide.TOP;
+        else if (side.equals("bottom"))
+            lookAt = BoxSide.BOTTOM;
 
         final TileEntity blockTileEntity = world.getBlockTileEntity(
                 computer.getX() + Facing.offsetsXForSide[lookAt],
