6212e4028a3b0f6fe9ae8472e9405a8762176076
marcins78@gmail.com <marcins78@gmail.com@4b48f6f3-f5b4-2729-cc71-6049987250f3>
2013-04-18 20:23:19 +0000

    Working on console.

--- com/gempukku/minecraft/automation/gui/computer/console/ComputerConsoleGui.java
+++ com/gempukku/minecraft/automation/gui/computer/console/ComputerConsoleGui.java
@@ -25,12 +25,14 @@ public class ComputerConsoleGui extends GuiScreen {
     private static final int BUTTON_PADDING_HOR = 3;
     private static final int BUTTON_PADDING_VER = 3;
 
+    private static final int FONT_HEIGHT=9;
     private static final int BLINK_LENGTH = 20;
     public static final String PLAYER_CONSOLE_TEXT = "Player console";
     public static final String COMPUTER_CONSOLE_TEXT = "Computer console";
 
-    // mode=0 is user (player) console, mode=1 is edit program mode, mode=2 is program output console
+    // mode=0 is user (player) console, mode=1 is program output console
     private int _mode = 0;
+    private boolean _editingProgram;
 
     private int _screenX;
     private int _screenY;
@@ -70,7 +72,7 @@ public class ComputerConsoleGui extends GuiScreen {
     public void initGui() {
         _characterWidth = 6;
         _computerScreenWidth = _characterWidth * ComputerConsole.CONSOLE_WIDTH;
-        _computerScreenHeight = fontRenderer.FONT_HEIGHT * ComputerConsole.CONSOLE_HEIGHT;
+        _computerScreenHeight = FONT_HEIGHT * ComputerConsole.CONSOLE_HEIGHT;
     }
 
     @Override
@@ -79,7 +81,7 @@ public class ComputerConsoleGui extends GuiScreen {
         drawDefaultBackground();
 
         _screenWidth = _computerScreenWidth + PADDING_HOR * 2;
-        _screenHeight = _computerScreenHeight + PADDING_VER * 2 + BUTTON_PADDING_VER * 2 + fontRenderer.FONT_HEIGHT;
+        _screenHeight = _computerScreenHeight + PADDING_VER * 2 + BUTTON_PADDING_VER * 2 + FONT_HEIGHT;
 
         _screenX = (width - _screenWidth) / 2;
         _screenY = (height - _screenHeight) / 2;
@@ -103,7 +105,7 @@ public class ComputerConsoleGui extends GuiScreen {
         drawVerticalLine(0, 0, _screenHeight, FRAME_COLOR);
         drawVerticalLine(_screenWidth, 0, _screenHeight, FRAME_COLOR);
 
-        int buttonHeight = BUTTON_PADDING_VER * 2 + fontRenderer.FONT_HEIGHT;
+        int buttonHeight = BUTTON_PADDING_VER * 2 + FONT_HEIGHT;
 
         int playerConsoleButtonWidth = fontRenderer.getStringWidth(PLAYER_CONSOLE_TEXT) + BUTTON_PADDING_HOR * 2;
         int computerConsoleButtonWidth = fontRenderer.getStringWidth(COMPUTER_CONSOLE_TEXT) + BUTTON_PADDING_HOR * 2;
@@ -113,8 +115,8 @@ public class ComputerConsoleGui extends GuiScreen {
         boolean computerConsoleHover = mouseX >= PADDING_HOR + playerConsoleButtonWidth && mouseX < PADDING_HOR + playerConsoleButtonWidth + computerConsoleButtonWidth
                 && mouseY >= PADDING_VER && mouseY < PADDING_VER + buttonHeight;
 
-        int playerConsoleButBgColor = getButBgColor(playerConsoleHover, (_mode == 0 || _mode == 1));
-        int computerConsoleButBgColor = getButBgColor(computerConsoleHover, (_mode == 2));
+        int playerConsoleButBgColor = getButBgColor(playerConsoleHover, _mode == 0);
+        int computerConsoleButBgColor = getButBgColor(computerConsoleHover, _mode == 1);
 
         // Draw button backgrounds
         drawRect(PADDING_HOR, PADDING_VER, PADDING_HOR + playerConsoleButtonWidth, PADDING_VER + buttonHeight, playerConsoleButBgColor);
@@ -129,12 +131,10 @@ public class ComputerConsoleGui extends GuiScreen {
 
         GL11.glPushMatrix();
         try {
-            GL11.glTranslatef(PADDING_HOR, PADDING_VER + BUTTON_PADDING_VER * 2 + fontRenderer.FONT_HEIGHT, 0);
+            GL11.glTranslatef(PADDING_HOR, PADDING_VER + BUTTON_PADDING_VER * 2 + FONT_HEIGHT, 0);
             if (_mode == 0)
                 drawPlayerConsole(timeSinceLastTick);
             else if (_mode == 1)
-                drawEditProgramConsole(timeSinceLastTick);
-            else if (_mode == 2)
                 drawComputerConsole(timeSinceLastTick);
         } finally {
             GL11.glPopMatrix();
@@ -142,21 +142,35 @@ public class ComputerConsoleGui extends GuiScreen {
     }
 
     private void drawPlayerConsole(float timeSinceLastTick) {
-        final String[] consoleLines = _playerConsole.getLines();
-        // Draw all lines but first (we need to fill current edited line at the bottom)
-        for (int i = 1; i < consoleLines.length; i++)
-            drawMonospacedLine(consoleLines[i], 0, (i - 1) * fontRenderer.FONT_HEIGHT, PLAYER_CONSOLE_TEXT_COLOR);
+        if (_editingProgram) {
+            drawEditProgramConsole(timeSinceLastTick);
+        } else {
+            final String[] consoleLines = _playerConsole.getLines();
+            // Draw all lines but first (we need to fill current edited line at the bottom)
+            for (int i = 1; i < consoleLines.length; i++)
+                drawMonospacedLine(consoleLines[i], 0, (i - 1) * FONT_HEIGHT, PLAYER_CONSOLE_TEXT_COLOR);
 
-        String wholeCommandLine = ">" + _currentCommand.toString();
-        String commandLine = wholeCommandLine.substring(_currentCommandDisplayStartIndex, Math.min(_currentCommandDisplayStartIndex + ComputerConsole.CONSOLE_WIDTH, wholeCommandLine.length()));
-        int cursorPositionInDisplayedCommandLine = 1 + _cursorPositionInPlayerCommand + _currentCommandDisplayStartIndex;
+            String wholeCommandLine = ">" + _currentCommand.toString();
+            String commandLine = wholeCommandLine.substring(_currentCommandDisplayStartIndex, Math.min(_currentCommandDisplayStartIndex + ComputerConsole.CONSOLE_WIDTH, wholeCommandLine.length()));
+            int cursorPositionInDisplayedCommandLine = 1 + _cursorPositionInPlayerCommand + _currentCommandDisplayStartIndex;
 
-        final int lastLineY = fontRenderer.FONT_HEIGHT * (ComputerConsole.CONSOLE_HEIGHT - 1);
-        drawMonospacedLine(commandLine, 0, lastLineY, PLAYER_CONSOLE_TEXT_COLOR);
+            final int lastLineY = FONT_HEIGHT * (ComputerConsole.CONSOLE_HEIGHT - 1);
+            drawMonospacedLine(commandLine, 0, lastLineY, PLAYER_CONSOLE_TEXT_COLOR);
 
-        _blinkTick = ((++_blinkTick) % BLINK_LENGTH);
-        if (_blinkTick * 2 > BLINK_LENGTH)
-            drawVerticalLine(1+cursorPositionInDisplayedCommandLine * _characterWidth, lastLineY, lastLineY + fontRenderer.FONT_HEIGHT, PLAYER_CONSOLE_TEXT_COLOR);
+            _blinkTick = ((++_blinkTick) % BLINK_LENGTH);
+            if (_blinkTick * 2 > BLINK_LENGTH)
+                drawVerticalLine(1 + cursorPositionInDisplayedCommandLine * _characterWidth, lastLineY, lastLineY + FONT_HEIGHT, PLAYER_CONSOLE_TEXT_COLOR);
+        }
+    }
+
+    private void drawEditProgramConsole(float timeSinceLastTick) {
+
+    }
+
+    private void drawComputerConsole(float timeSinceLastTick) {
+        final String[] consoleLines = _computerConsole.getLines();
+        for (int i = 0; i < consoleLines.length; i++)
+            drawMonospacedLine(consoleLines[i], 0, i * FONT_HEIGHT, COMPUTER_CONSOLE_TEXT_COLOR);
     }
 
     private void drawMonospacedLine(String line, int x, int y, int color) {
@@ -192,21 +206,11 @@ public class ComputerConsoleGui extends GuiScreen {
         GL11.glEnd();
     }
 
-    private void drawEditProgramConsole(float timeSinceLastTick) {
-
-    }
-
-    private void drawComputerConsole(float timeSinceLastTick) {
-        final String[] consoleLines = _computerConsole.getLines();
-        for (int i = 0; i < consoleLines.length; i++)
-            drawMonospacedLine(consoleLines[i], 0, i * fontRenderer.FONT_HEIGHT, COMPUTER_CONSOLE_TEXT_COLOR);
-    }
-
     @Override
     protected void mouseClicked(int mouseX, int mouseY, int which) {
-        if ((_mode == 0 || _mode == 1) && _computerConsoleModeButton.contains(mouseX - _screenX, mouseY - _screenY))
-            _mode = 2;
-        else if (_mode == 2 && _playerConsoleModeButton.contains(mouseX - _screenX, mouseY - _screenY))
+        if (_mode == 0 && _computerConsoleModeButton.contains(mouseX - _screenX, mouseY - _screenY))
+            _mode = 1;
+        else if (_mode == 1 && _playerConsoleModeButton.contains(mouseX - _screenX, mouseY - _screenY))
             _mode = 0;
     }
 
