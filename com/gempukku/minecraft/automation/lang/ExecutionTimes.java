1924cb8e8bf785849ced4046e65c03470b8e5e38
marcins78@gmail.com <marcins78@gmail.com@4b48f6f3-f5b4-2729-cc71-6049987250f3>
2013-04-21 07:10:06 +0000

    Setting speed of some operations.

--- com/gempukku/minecraft/automation/lang/ExecutionTimes.java
+++ com/gempukku/minecraft/automation/lang/ExecutionTimes.java
@@ -1,17 +1,21 @@
 package com.gempukku.minecraft.automation.lang;
 
 public class ExecutionTimes {
-	public static final int STACK_EXECUTION = 10;
-	public static final int GET_CONTEXT_VALUE = 10;
-	public static final int SET_VARIABLE = 10;
+	public static final int GET_CONTEXT_VALUE = 1;
+	public static final int SET_CONTEXT_VALUE = 1;
+	public static final int GET_RETURN_VALUE = 1;
+	public static final int SET_RETURN_VALUE = 1;
+
+	public static final int BREAK_BLOCK = 2;
+	public static final int DEFINE_VARIABLE = 2;
+	public static final int SET_VARIABLE = 2;
+
+	public static final int STACK_EXECUTION = 5;
+	public static final int STACK_GROUP_EXECUTION = 8;
+
 	public static final int SUM_VALUES = 10;
 	public static final int OTHER_MATH_OPERATION = 10;
-	public static final int STACK_GROUP_EXECUTION = 10;
-	public static final int BREAK_BLOCK = 10;
 	public static final int COMPARE_VALUES = 10;
-	public static final int SET_CONTEXT_VALUE = 10;
-	public static final int DEFINE_VARIABLE = 10;
+
 	public static final int RESOLVE_MEMBER = 10;
-	public static final int GET_RETURN_VALUE = 10;
-	public static final int SET_RETURN_VALUE = 10;
 }
