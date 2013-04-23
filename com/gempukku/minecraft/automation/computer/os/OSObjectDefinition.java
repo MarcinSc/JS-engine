3c48408b5fa2471a63ac4f53951afa1abe6acba8
marcins78@gmail.com <marcins78@gmail.com@4b48f6f3-f5b4-2729-cc71-6049987250f3>
2013-04-23 17:33:17 +0000

    Added parseInt() function to os.

--- com/gempukku/minecraft/automation/computer/os/OSObjectDefinition.java
+++ com/gempukku/minecraft/automation/computer/os/OSObjectDefinition.java
@@ -6,11 +6,14 @@ import com.gempukku.minecraft.automation.lang.Variable;
 
 public class OSObjectDefinition implements ObjectDefinition {
 	private Variable _parseFloat = new Variable(new ParseFloatFunction());
+	private Variable _parseInt = new Variable(new ParseIntFunction());
 
 	@Override
 	public Variable getMember(ExecutionContext context, String name) {
 		if (name.equals("parseFloat"))
 			return _parseFloat;
+		else if (name.equals("parseInt"))
+			return _parseInt;
 
 		return new Variable(null);
 	}
